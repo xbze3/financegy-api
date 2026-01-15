fcc4f44e81d450172e2b19b2009810f810655095
xbze3 <ezranminty@gmail.com>
2026-01-15 01:16:35 -0400

    Include securities router in main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
 from fastapi import FastAPI
+from routes import securities
+
+app = FastAPI(title="FinanceGY-API", root_path="/v1")
+
+app.include_router(securities.router)
--- app/routes/securities.py
+++ app/routes/securities.py
@@ -7,3 +7,8 @@ router = APIRouter(tags=["securities"])
 @router.get("/securities")
 def get_securities():
     return financegy.get_securities()
+
+
+@router.get("/securities/{symbol}")
+def get_security_by_symbol(symbol: str):
+    return financegy.get_security_by_symbol(symbol)
